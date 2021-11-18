044c333747de509a2747ce1290b22a4f4c8cf247
kumbamshyam <shyammudiraj955@gmail.com>
2021-11-18 20:53:29 +0530

    u

---
+++ tut69.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+const port = 80;
+
+
+// for serving the static file
+app.use('/static', express.static('static'));
+
+// set the template engine as pug
+app.set('view engine', 'pug');
+
+// set the view directory
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res)=>{
+    res.send('You opened home page');
+});
+
+
+app.get('/about', (req, res)=>{
+    res.send('You opened about page');
+});
+
+app.listen(port, ()=>{
+    console.log(`this application is started successfully on port ${port} `)
+})
